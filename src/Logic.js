fe8bcb333eb31bbc6d389b4a6e4bf0f499defe4d
chichobanche <kostadin.pejoski1@gmail.com>
2022-08-26 13:49:02 +0200

    implemented the dynamic loading of Todos

--- src/Logic.js
+++ src/Logic.js
@@ -2,15 +2,89 @@ export default function clearElement(element){
     element.innerHTML="";
 }
 
+import { grid } from ".";
+
+let todoTitleInput=document.querySelector(`[name="todoTitle"]`) || null;
+let todoTextInput= document.querySelector(`[name="todoText"]`) || null;
+let todoPriortyInput=document.querySelector(`[name="todoPriorty"]`) || null;
+
 export class toDo{
-    constructor(title,dueDate,priorty){
+    constructor(title,dueDate,priorty,text){
         this.title=title;
         this.dueDate=dueDate;
         this.priorty=priorty;
         this.actualElement="";
+        this.text=text;
     }
     load(){
-        return;
+        let todo=document.createElement("div");
+        todo.classList.add("todo");
+        let placeholdeText=document.createElement("p");
+        placeholdeText.classList.add("todoText");
+        placeholdeText.textContent=this.text;
+        let placeholderTitle=document.createElement("p");
+        placeholderTitle.classList.add("todoTitle");
+        placeholderTitle.textContent=this.title;
+        let placeholderPriorty=document.createElement("div");
+        placeholderPriorty.classList.add("todoPriorty");
+
+        for(let i=0;i<3;i++){
+            let text="";
+            let className="";
+            let el=document.createElement("p");
+            if(i==0){
+                el.id="0";
+                text="low";
+                className="green";
+            }
+            else if(i==1){
+                el.id="1";
+                text="medium";
+                className="yellow";
+    
+            }
+            else{
+                el.id="2";
+                text="high";
+                className="red";
+            }
+            
+            if(this.priorty==text){
+                switch(this.priorty){
+                    case "low":{
+                        el.classList.add("green");
+                        break;
+                    }
+                    case "medium":{
+                        el.classList.add("yellow");
+                        break;
+                    }
+                    case "high":{
+                        el.classList.add("red");
+                        break;
+                    }
+                }
+            }
+
+
+            el.classList.add("priortyEl");
+            el.textContent=text;
+            placeholderPriorty.append(el);
+        }
+
+        const closeBtn=document.createElement("p");
+        closeBtn.textContent="X";
+        closeBtn.classList.add("closeBtn");
+        closeBtn.addEventListener("click",closeBtnFnc);
+
+        todo.append(placeholderTitle,closeBtn,placeholdeText,placeholderPriorty);
+
+        grid.append(todo);
+        
+        const priortyEls=document.querySelectorAll(".priortyEl");
+        priortyEls.forEach(El=>{
+            El.addEventListener("click",swtichClasses);
+        })
     }
 };
 
@@ -19,6 +93,7 @@ export class Project{
     constructor(title) {
         this.title=title;
         this.todos=[];
+        this.isLoaded=false;
     }
     loadTodos(){
         this.todos.forEach(todo=>{
@@ -28,6 +103,37 @@ export class Project{
 }
 
 export  function closeBtnFnc(){
-    console.log(this)
     this.parentElement.remove();
+}
+
+function swtichClasses(){
+    if(this.classList.contains("red") || this.classList.contains("yellow") || this.classList.contains("green")){
+        console.log("ok");
+        return;
+    }
+    else if(this.id=="0"){
+        this.classList.add("green");
+        let second=this.nextElementSibling;
+        second.classList.remove("yellow");
+
+        let third=second.nextElementSibling;
+        console.log(third);
+        third.classList.remove("red");
+    }
+
+    else if(this.id=="1"){ 
+        this.classList.add("yellow");
+        let first=this.previousElementSibling;
+        first.classList.remove("green");
+        let third=this.nextElementSibling;
+        third.classList.remove("red");
+    }
+    else{
+        this.classList.add("red");
+        let second=this.previousElementSibling;
+        second.classList.remove("yellow");
+        let first=second.previousElementSibling;
+        first.classList.remove("green");
+        
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 // imports
 import "./file.css";
-import clearElement from "./Logic";
+import clearElement, { Project } from "./Logic";
 import { toDo } from "./Logic";
 import { closeBtnFnc } from "./Logic";
 
@@ -9,6 +9,7 @@ import { closeBtnFnc } from "./Logic";
 // creating all the elements
 // creating the header
 
+let testProject= new Project("test");
 const header=document.createElement("div");
 const sidebar=document.createElement("div");
 export const main=document.createElement("div");
@@ -26,57 +27,58 @@ header.classList.add("header");
 // creating the sidebar
 const creationEl=document.createElement("div");
 creationEl.classList.add("creationEl");
-const makeTodo=document.createElement("p");
-makeTodo.textContent="New Todo";
-const makeProject=document.createElement("p");
-makeProject.textContent="Make New Project";
-creationEl.append(makeTodo,makeProject);
+export const grid=document.createElement("div");
+grid.classList.add("grid");
 const projectsList=document.createElement("div");
 projectsList.classList.add("projects");
-
+const currentProjectTitle=document.createElement("h1");
+currentProjectTitle.textContent="Current Project";
+currentProjectTitle.classList.add("projectTitle");
+currentProjectTitle.classList.add("currentProjectTitle");
+main.append(currentProjectTitle,grid);
 sidebar.append(creationEl);
 sidebar.append(projectsList);
 
 // placeholder Todos
-for(let i=0;i<20;i++){
-    let todo=document.createElement("div");
-    todo.classList.add("todo");
-    let placeholdeText=document.createElement("p");
-    placeholdeText.classList.add("todoText");
-    placeholdeText.textContent="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, beatae adipisci! Fuga magnam accusantium optio recusandae illo asperiores, fugiat eum!";
-    let placeholderTitle=document.createElement("p");
-    placeholderTitle.classList.add("todoTitle");
-    placeholderTitle.textContent="PlaceHolderTitle";
-    let placeholderPriorty=document.createElement("div");
-    placeholderPriorty.classList.add("todoPriorty");
-    for(let i=0;i<3;i++){
-        let text="";
-        let className="";
-        let el=document.createElement("p");
-        if(i==0){
-            text="Low";
-            className="green";
-        }
-        else if(i==1){
-            text="Medium";
-            className="yellow";
+// for(let i=0;i<5;i++){
+//     let todo=document.createElement("div");
+//     todo.classList.add("todo");
+//     let placeholdeText=document.createElement("p");
+//     placeholdeText.classList.add("todoText");
+//     placeholdeText.textContent="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, beatae adipisci! Fuga magnam accusantium optio recusandae illo asperiores, fugiat eum!";
+//     let placeholderTitle=document.createElement("p");
+//     placeholderTitle.classList.add("todoTitle");
+//     placeholderTitle.textContent="PlaceHolderTitle";
+//     let placeholderPriorty=document.createElement("div");
+//     placeholderPriorty.classList.add("todoPriorty");
+//     for(let i=0;i<3;i++){
+//         let text="";
+//         let className="";
+//         let el=document.createElement("p");
+//         if(i==0){
+//             text="low";
+//             className="green";
+//         }
+//         else if(i==1){
+//             text="Medium";
+//             className="yellow";
 
-        }
-        else{
-            text="High";
-            className="red";
-        }
-        el.classList.add(className);
-        el.classList.add("priortyEl");
-        el.textContent=text;
-        placeholderPriorty.append(el)
-    }
-    const closeBtn=document.createElement("p");
-    closeBtn.textContent="X";
-    closeBtn.classList.add("closeBtn");
-    todo.append(placeholderTitle,closeBtn,placeholdeText,placeholderPriorty);
-    main.append(todo);
-}
+//         }
+//         else{
+//             text="High";
+//             className="red";
+//         }
+//         el.classList.add(className);
+//         el.classList.add("priortyEl");
+//         el.textContent=text;
+//         placeholderPriorty.append(el);
+//     }
+//     const closeBtn=document.createElement("p");
+//     closeBtn.textContent="X";
+//     closeBtn.classList.add("closeBtn");
+//     todo.append(placeholderTitle,closeBtn,placeholdeText,placeholderPriorty);
+//     grid.append(todo);
+// }
 
 
 sidebar.classList.add("sidebar");
@@ -87,4 +89,66 @@ document.body.append(header,sidebar,main);
 const closeBtns=document.querySelectorAll(".closeBtn");
 closeBtns.forEach(closeBtn=>closeBtn.addEventListener("click",closeBtnFnc));
 
-console.clear()
+console.clear();
+
+function generateTodoForm(){
+    clearElement(creationEl);
+    const form=document.createElement("div");
+    form.classList.add("form");
+    const todoTitleInput=document.createElement("input");
+    todoTitleInput.name="todoTitle";
+    todoTitleInput.placeholder="The title of your todo";
+    const todoTextInput=document.createElement("input");
+    todoTextInput.placeholder="Insert what you have to do";
+    todoTextInput.name="todoText";
+    const select=document.createElement("select");
+    select.id="select";
+    select.textContent="Select the priorty of the todo";
+    select.name="todoPriorty";
+    const labelForSelect=document.createElement("label");
+    labelForSelect.for="select";
+    labelForSelect.textContent="Priority:"
+    const low=document.createElement("option");
+    low.textContent="low";
+    low.value="low";
+    const medium=document.createElement("option");
+    medium.value="medium";
+    medium.textContent="medium";
+    const high=document.createElement("option");
+    high.value="high";
+    high.textContent="high";
+    const createBtn=document.createElement("button");
+    createBtn.textContent="create new Todo";
+    createBtn.addEventListener("click",()=>{
+        let todo = new toDo(todoTitleInput.value,"09/09/2003",select.value,todoTextInput.value);
+
+        testProject.todos.push(todo);
+        console.log(testProject);
+        todo.load();
+        generateOptions();
+    });
+
+    select.append(low,medium,high);
+
+    form.append(todoTitleInput,todoTextInput,labelForSelect,select,createBtn);
+
+    creationEl.append(form);
+
+
+}
+
+function generateOptions(){
+    clearElement(creationEl);
+    const makeTodo=document.createElement("p");
+    makeTodo.textContent="New Todo";
+    makeTodo.addEventListener("click",generateTodoForm);
+    const makeProject=document.createElement("p");
+    makeProject.textContent="Make New Project";
+    creationEl.append(makeTodo,makeProject);
+}
+
+generateOptions();
+
+
+
+
