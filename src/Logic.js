af8e3e63a5eb98827520770c36061fabf55905ba
chichobanche <kostadin.pejoski1@gmail.com>
2022-08-29 22:08:48 +0200

    added a linter

--- src/Logic.js
+++ src/Logic.js
@@ -1,174 +1,159 @@
-export default function clearElement(element){
-    element.innerHTML="";
+import { grid, projects } from '.'
+
+export default function clearElement (element) {
+  element.innerHTML = ''
 }
 
-import { grid } from ".";
-import { projects } from ".";
+export class toDo {
+  constructor (title, dueDate, priorty, text) {
+    this.title = title
+    this.dueDate = dueDate
+    this.priorty = priorty
+    this.actualElement = ''
+    this.text = text
+    this.parentArr = []
+  }
 
-export class toDo{
-    constructor(title,dueDate,priorty,text){
-        this.title=title;
-        this.dueDate=dueDate;
-        this.priorty=priorty;
-        this.actualElement="";
-        this.text=text;
-        this.parentArr=[];
+  deleteYourself () {
+    console.log(this.parentArr)
+    for (let i = 0; i < this.parentArr.length; i++) {
+      if (this.parentArr[i] == this) {
+        this.parentArr.splice(i, 1)
+      }
     }
+  }
 
-    deleteYourself(){
-        console.log(this.parentArr);
-        for(let i=0;i<this.parentArr.length;i++){
+  changePriorty (newPriorty) {
+    this.priorty = newPriorty
+  }
 
-            if(this.parentArr[i]==this){
-                this.parentArr.splice(i,1);
-            }
+  load () {
+    console.log(this)
+    const todo = document.createElement('div')
+    todo.classList.add('todo')
+    const placeholdeText = document.createElement('p')
+    placeholdeText.classList.add('todoText')
+    placeholdeText.textContent = this.text
+    const placeholderTitle = document.createElement('p')
+    placeholderTitle.classList.add('todoTitle')
+    placeholderTitle.textContent = this.title
+    const placeholderPriorty = document.createElement('div')
+    placeholderPriorty.classList.add('todoPriorty')
+
+    for (let i = 0; i < 3; i++) {
+      let text = ''
+      let className = ''
+      const el = document.createElement('p')
+      if (i == 0) {
+        el.id = '0'
+        text = 'low'
+        className = 'green'
+      } else if (i == 1) {
+        el.id = '1'
+        text = 'medium'
+        className = 'yellow'
+      } else {
+        el.id = '2'
+        text = 'high'
+        className = 'red'
+      }
+
+      if (this.priorty == text) {
+        switch (this.priorty) {
+          case 'low':{
+            el.classList.add('green')
+            break
+          }
+          case 'medium':{
+            el.classList.add('yellow')
+            break
+          }
+          case 'high':{
+            el.classList.add('red')
+            break
+          }
         }
+      }
+
+      el.classList.add('priortyEl')
+      el.textContent = text
+      placeholderPriorty.append(el)
     }
 
-    changePriorty(newPriorty){
-        this.priorty=newPriorty;
-    }
+    const closeBtn = document.createElement('p')
+    closeBtn.textContent = 'X'
+    closeBtn.classList.add('closeBtn')
+    closeBtn.addEventListener('click', closeBtnFnc)
+    closeBtn.addEventListener('click', () => {
+      this.deleteYourself()
+    })
 
-    load(){
-        console.log(this);
-        let todo=document.createElement("div");
-        todo.classList.add("todo");
-        let placeholdeText=document.createElement("p");
-        placeholdeText.classList.add("todoText");
-        placeholdeText.textContent=this.text;
-        let placeholderTitle=document.createElement("p");
-        placeholderTitle.classList.add("todoTitle");
-        placeholderTitle.textContent=this.title;
-        let placeholderPriorty=document.createElement("div");
-        placeholderPriorty.classList.add("todoPriorty");
+    todo.append(placeholderTitle, closeBtn, placeholdeText, placeholderPriorty)
 
-        for(let i=0;i<3;i++){
-            let text="";
-            let className="";
-            let el=document.createElement("p");
-            if(i==0){
-                el.id="0";
-                text="low";
-                className="green";
-            }
-            else if(i==1){
-                el.id="1";
-                text="medium";
-                className="yellow";
-    
-            }
-            else{
-                el.id="2";
-                text="high";
-                className="red";
-            }
-            
-            if(this.priorty==text){
-                switch(this.priorty){
-                    case "low":{
-                        el.classList.add("green");
-                        break;
-                    }
-                    case "medium":{
-                        el.classList.add("yellow");
-                        break;
-                    }
-                    case "high":{
-                        el.classList.add("red");
-                        break;
-                    }
-                }
-            }
+    grid.append(todo)
 
-
-            el.classList.add("priortyEl");
-            el.textContent=text;
-            placeholderPriorty.append(el);
+    const priortyEls = document.querySelectorAll('.priortyEl')
+    for (let i = 0; i < priortyEls.length; i++) {
+      priortyEls[i].addEventListener('click', swtichClasses)
+      priortyEls[i].addEventListener('click', () => {
+        if (priortyEls[i].id == '0') {
+          this.changePriorty('low')
+        } else if (priortyEls[i].id == '1') {
+          this.changePriorty('medium')
+        } else if (priortyEls[i].id == '2') {
+          this.changePriorty('high')
         }
-
-        const closeBtn=document.createElement("p");
-        closeBtn.textContent="X";
-        closeBtn.classList.add("closeBtn");
-        closeBtn.addEventListener("click",closeBtnFnc);
-        closeBtn.addEventListener("click",()=>{
-            this.deleteYourself();
-        });
-
-        todo.append(placeholderTitle,closeBtn,placeholdeText,placeholderPriorty);
-
-        grid.append(todo);
-
-        const priortyEls=document.querySelectorAll(".priortyEl");
-        for(let i=0;i<priortyEls.length;i++){
-            priortyEls[i].addEventListener("click",swtichClasses);
-            priortyEls[i].addEventListener("click",()=>{
-                if(priortyEls[i].id=="0"){
-                    this.changePriorty("low");
-
-                }
-                else if(priortyEls[i].id=="1"){
-                    this.changePriorty("medium");
-
-                }
-                else if(priortyEls[i].id=="2"){
-                    this.changePriorty("high");
-                }
-            })
-        }
+      })
     }
+  }
 };
 
+export class Project {
+  constructor (title) {
+    this.title = title
+    this.todos = []
+    this.isLoaded = false
+    this.domParagraph = ''
+    this.deleteButton = ''
+  }
 
-export class Project{
-    constructor(title) {
-        this.title=title;
-        this.todos=[];
-        this.isLoaded=false;
+  loadTodos () {
+    clearElement(grid)
+    if (this.isLoaded) {
+      this.todos.forEach(todo => {
+        todo.load()
+      })
+    } else {
+
     }
-    loadTodos(){
-        clearElement(grid);
-        if(this.isLoaded){
-            this.todos.forEach(todo=>{
-                todo.load();
-            })
-        }
-        else{
-            return;
-        }
-    }
+  }
 }
 
-export  function closeBtnFnc(){
-    this.parentElement.remove();
+export function closeBtnFnc () {
+  this.parentElement.remove()
 }
 
-function swtichClasses(){
-    if(this.classList.contains("red") || this.classList.contains("yellow") || this.classList.contains("green")){
-        return;
-    }
-    else if(this.id=="0"){
-        this.classList.add("green");
-        let second=this.nextElementSibling;
-        second.classList.remove("yellow");
+function swtichClasses () {
+  if (this.classList.contains('red') || this.classList.contains('yellow') || this.classList.contains('green')) {
 
-        let third=second.nextElementSibling;
-        third.classList.remove("red");
-    }
+  } else if (this.id == '0') {
+    this.classList.add('green')
+    const second = this.nextElementSibling
+    second.classList.remove('yellow')
 
-    else if(this.id=="1"){
-
-        this.classList.add("yellow");
-        let first=this.previousElementSibling;
-        first.classList.remove("green");
-        let third=this.nextElementSibling;
-        third.classList.remove("red");
-    }
-    else{
-        this.classList.add("red");
-        let second=this.previousElementSibling;
-        second.classList.remove("yellow");
-        let first=second.previousElementSibling;
-        first.classList.remove("green");
-        
-    }
+    const third = second.nextElementSibling
+    third.classList.remove('red')
+  } else if (this.id == '1') {
+    this.classList.add('yellow')
+    const first = this.previousElementSibling
+    first.classList.remove('green')
+    const third = this.nextElementSibling
+    third.classList.remove('red')
+  } else {
+    this.classList.add('red')
+    const second = this.previousElementSibling
+    second.classList.remove('yellow')
+    const first = second.previousElementSibling
+    first.classList.remove('green')
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,235 +1,218 @@
-
 // imports
-import "./file.css";
-import clearElement, { Project } from "./Logic";
-import { toDo } from "./Logic";
-import { closeBtnFnc } from "./Logic";
+import './file.css'
+import clearElement, { Project, toDo, closeBtnFnc } from './Logic'
+
+console.clear()
 
 // main setup
 // creating all the elements
 // creating the header
 
-export let projects=[];
+export const projects = JSON.parse(localStorage.getItem('storageProjects'))
 
-const header=document.createElement("div");
-const sidebar=document.createElement("div");
-export const main=document.createElement("div");
-const headerh1=document.createElement("h1");
-const wrapper=document.createElement("div");
-wrapper.id="wraper";
-const madeBy=document.createElement("p");
-madeBy.textContent="Made by Kosta Bratce";
-headerh1.textContent="Todo List App";
-wrapper.append(headerh1);
-wrapper.append(madeBy);
+console.log(projects)
 
-header.append(wrapper);
-header.classList.add("header");
+const header = document.createElement('div')
+const sidebar = document.createElement('div')
+export const main = document.createElement('div')
+const headerh1 = document.createElement('h1')
+const wrapper = document.createElement('div')
+wrapper.id = 'wraper'
+const madeBy = document.createElement('p')
+madeBy.textContent = 'Made by Kosta'
+headerh1.textContent = 'Todo List App'
+wrapper.append(headerh1)
+wrapper.append(madeBy)
+
+header.append(wrapper)
+header.classList.add('header')
 // creating the sidebar
-const creationEl=document.createElement("div");
-creationEl.classList.add("creationEl");
-export const grid=document.createElement("div");
-grid.classList.add("grid");
-const projectsList=document.createElement("div");
-projectsList.classList.add("projects");
-const currentProjectTitle=document.createElement("h1");
-currentProjectTitle.classList.add("currentProjectTitle");
-main.append(currentProjectTitle,grid);
-sidebar.append(creationEl,projectsList);
+const creationEl = document.createElement('div')
+creationEl.classList.add('creationEl')
+export const grid = document.createElement('div')
+grid.classList.add('grid')
+const projectsList = document.createElement('div')
+projectsList.classList.add('projects')
+const currentProjectTitle = document.createElement('h1')
+currentProjectTitle.classList.add('currentProjectTitle')
+main.append(currentProjectTitle, grid)
+sidebar.append(creationEl, projectsList)
 
+sidebar.classList.add('sidebar')
+main.classList.add('main')
 
-// placeholder Todos
-// for(let i=0;i<5;i++){
-//     let todo=document.createElement("div");
-//     todo.classList.add("todo");
-//     let placeholdeText=document.createElement("p");
-//     placeholdeText.classList.add("todoText");
-//     placeholdeText.textContent="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, beatae adipisci! Fuga magnam accusantium optio recusandae illo asperiores, fugiat eum!";
-//     let placeholderTitle=document.createElement("p");
-//     placeholderTitle.classList.add("todoTitle");
-//     placeholderTitle.textContent="PlaceHolderTitle";
-//     let placeholderPriorty=document.createElement("div");
-//     placeholderPriorty.classList.add("todoPriorty");
-//     for(let i=0;i<3;i++){
-//         let text="";
-//         let className="";
-//         let el=document.createElement("p");
-//         if(i==0){
-//             text="low";
-//             className="green";
-//         }
-//         else if(i==1){
-//             text="Medium";
-//             className="yellow";
+document.body.append(header, sidebar, main)
 
-//         }
-//         else{
-//             text="High";
-//             className="red";
-//         }
-//         el.classList.add(className);
-//         el.classList.add("priortyEl");
-//         el.textContent=text;
-//         placeholderPriorty.append(el);
-//     }
-//     const closeBtn=document.createElement("p");
-//     closeBtn.textContent="X";
-//     closeBtn.classList.add("closeBtn");
-//     todo.append(placeholderTitle,closeBtn,placeholdeText,placeholderPriorty);
-//     grid.append(todo);
-// }
+const closeBtns = document.querySelectorAll('.closeBtn')
+closeBtns.forEach(closeBtn => closeBtn.addEventListener('click', closeBtnFnc))
 
+function generateTodoForm () {
+  if (projects.length == 0) {
+    alert("you can't make todos without parent projects")
+    generateOptions()
+    return
+  }
+  clearElement(creationEl)
+  const form = document.createElement('div')
+  form.classList.add('form')
+  const todoTitleInput = document.createElement('input')
+  todoTitleInput.name = 'todoTitle'
+  todoTitleInput.placeholder = 'The title of your todo'
+  const todoTextInput = document.createElement('input')
+  todoTextInput.placeholder = 'Insert what you have to do'
+  todoTextInput.name = 'todoText'
+  const select = document.createElement('select')
+  select.id = 'select'
+  select.textContent = 'Select the priorty of the todo'
+  select.name = 'todoPriorty'
+  const labelForSelect = document.createElement('label')
+  labelForSelect.for = 'select'
+  labelForSelect.textContent = 'Priority:'
+  const low = document.createElement('option')
+  low.textContent = 'low'
+  low.value = 'low'
+  const medium = document.createElement('option')
+  medium.value = 'medium'
+  medium.textContent = 'medium'
+  const high = document.createElement('option')
+  high.value = 'high'
+  high.textContent = 'high'
+  const createBtn = document.createElement('button')
+  createBtn.textContent = 'create new Todo'
+  createBtn.addEventListener('click', () => {
+    if (todoTitleInput.value == '' || todoTextInput.value == '') {
+      alert('make sure to fill all the fields')
+      generateOptions()
+      return
+    }
+    const todo = new toDo(todoTitleInput.value, '09/09/2003', select.value, todoTextInput.value)
+    for (let i = 0; i < projects.length; i++) {
+      if (projects[i].isLoaded) {
+        todo.parentArr = projects[i].todos
+        projects[i].todos.push(todo)
+      }
+    }
 
-sidebar.classList.add("sidebar");
-main.classList.add("main");
+    todo.load()
+    generateOptions()
+  })
 
-document.body.append(header,sidebar,main);
+  select.append(low, medium, high)
 
-const closeBtns=document.querySelectorAll(".closeBtn");
-closeBtns.forEach(closeBtn=>closeBtn.addEventListener("click",closeBtnFnc));
+  form.append(todoTitleInput, todoTextInput, labelForSelect, select, createBtn)
 
-console.clear();
+  creationEl.append(form)
+}
 
-function generateTodoForm(){
-    clearElement(creationEl);
-    const form=document.createElement("div");
-    form.classList.add("form");
-    const todoTitleInput=document.createElement("input");
-    todoTitleInput.name="todoTitle";
-    todoTitleInput.placeholder="The title of your todo";
-    const todoTextInput=document.createElement("input");
-    todoTextInput.placeholder="Insert what you have to do";
-    todoTextInput.name="todoText";
-    const select=document.createElement("select");
-    select.id="select";
-    select.textContent="Select the priorty of the todo";
-    select.name="todoPriorty";
-    const labelForSelect=document.createElement("label");
-    labelForSelect.for="select";
-    labelForSelect.textContent="Priority:"
-    const low=document.createElement("option");
-    low.textContent="low";
-    low.value="low";
-    const medium=document.createElement("option");
-    medium.value="medium";
-    medium.textContent="medium";
-    const high=document.createElement("option");
-    high.value="high";
-    high.textContent="high";
-    const createBtn=document.createElement("button");
-    createBtn.textContent="create new Todo";
-    createBtn.addEventListener("click",()=>{
-        if(todoTitleInput.value=="" || todoTextInput.value==""){
-            alert("make sure to fill all the fields");
-            generateOptions();
-            return;
+function loadProjectForm () {
+  clearElement(creationEl)
+  const form = document.createElement('div')
+  form.classList.add('form')
+  const projectTitleInput = document.createElement('input')
+  projectTitleInput.placeholder = "Enter the project's name"
+  const projectBtn = document.createElement('button')
+  projectBtn.textContent = 'Create new Project'
+
+  form.append(projectTitleInput, projectBtn)
+  creationEl.append(form)
+  projectBtn.addEventListener('click', () => {
+    if (projectTitleInput.value == '') {
+      alert("you can't have empty title")
+      generateOptions()
+      return
+    }
+    currentProjectTitle.textContent = projectTitleInput.value
+    const newProject = new Project(projectTitleInput.value)
+    projects.forEach(project => {
+      if (project.isLoaded) {
+        project.isLoaded = false
+      }
+    })
+    newProject.isLoaded = true
+    const project = document.createElement('div')
+    const p = document.createElement('p')
+    const deleteProject = document.createElement('p')
+    deleteProject.textContent = 'X'
+    // deleting from Dom
+    deleteProject.addEventListener('click', deleteThisProject)
+    projects.push(newProject)
+    // deleting from array
+    deleteProject.addEventListener('click', () => {
+      for (let i = 0; i < projects.length; i++) {
+        if (currentProjectTitle.textContent == newProject.title) {
+          currentProjectTitle.textContent = ''
+          clearElement(grid)
         }
-        let todo = new toDo(todoTitleInput.value,"09/09/2003",select.value,todoTextInput.value);
-        for(let i=0;i<projects.length;i++){
-            if(projects[i].isLoaded){
-                todo.parentArr=projects[i].todos;
-                projects[i].todos.push(todo);
-            }
+        if (projects[i] == newProject) {
+          projects.splice(i, 1)
         }
-        
-        todo.load();
-        generateOptions();
-    });
-
-    select.append(low,medium,high);
-
-    form.append(todoTitleInput,todoTextInput,labelForSelect,select,createBtn);
-
-    creationEl.append(form);
-
-
-}
-
-function loadProjectForm(){
-    clearElement(creationEl);
-    const form=document.createElement("div");
-    form.classList.add("form");
-    const projectTitleInput=document.createElement("input");
-    projectTitleInput.placeholder="Enter the project's name";
-    const projectBtn=document.createElement("button");
-    projectBtn.textContent="Create new Project";
-
-    form.append(projectTitleInput,projectBtn);
-    creationEl.append(form);
-    projectBtn.addEventListener("click",()=>{
-        if(projectTitleInput.value==""){
-            alert("you can't have empty title");
-            generateOptions();
-            return;
+      }
+    })
+    project.classList.add('project')
+    project.append(p, deleteProject)
+    p.textContent = newProject.title
+    p.addEventListener('click', () => {
+      const currentTitle = p.textContent
+      currentProjectTitle.textContent = currentTitle
+      for (let i = 0; i < projects.length; i++) {
+        console.log(projects)
+        if (projects[i].title != currentTitle && projects[i].isLoaded) {
+          projects[i].isLoaded = false
+        } else if (projects[i].title == currentTitle) {
+          projects[i].isLoaded = true
+          projects[i].loadTodos()
         }
-        currentProjectTitle.textContent=projectTitleInput.value;
-        let newProject =new Project(projectTitleInput.value);
-        projects.forEach(project=>{
-            if(project.isLoaded){
-                project.isLoaded=false;
-            }
-        })
-        newProject.isLoaded=true;
-        let project=document.createElement("div");
-        let p=document.createElement("p");
-        let deleteProject=document.createElement("p");
-        deleteProject.textContent="X";
-        // deleting from Dom
-        deleteProject.addEventListener("click",deleteThisProject);
-        projects.push(newProject);
-        // deleting from array
-        deleteProject.addEventListener("click",()=>{
-            for(let i=0;i<projects.length;i++){
-                if(currentProjectTitle.textContent==newProject.title){
-                    currentProjectTitle.textContent="";
-                    clearElement(grid);
-                }
-                if(projects[i]==newProject){
-                    projects.splice(i,1);
-                }
-            }
-        });
-        project.classList.add("project");
-        project.append(p,deleteProject);
-        p.textContent=newProject.title;
-        p.addEventListener("click",()=>{
-            let currentTitle=p.textContent;
-            currentProjectTitle.textContent=currentTitle;
-            for(let i=0;i<projects.length;i++){
-                console.log(projects)
-                if(projects[i].title!=currentTitle && projects[i].isLoaded){
-                    projects[i].isLoaded=false;
-
-                }
-                else if(projects[i].title==currentTitle){
-                    projects[i].isLoaded=true;
-                    projects[i].loadTodos();
-
-                }
-            }
-        })
-        projectsList.append(project);
-        newProject.loadTodos();
-        generateOptions();
-    });
+      }
+    })
+    projectsList.append(project)
+    console.log(deleteProject, p)
+    newProject.deleteButton = deleteProject.innerHTML
+    newProject.domParagraph = p.innerHTML
+    console.log(newProject.deleteButton)
+    console.log(newProject.domParagraph)
+    newProject.loadTodos()
+    generateOptions()
+    localStorage.setItem('storageProjects', JSON.stringify(projects))
+  })
 }
 
-function generateOptions(){
-    clearElement(creationEl);
-    const makeTodo=document.createElement("p");
-    makeTodo.textContent="New Todo";
-    makeTodo.addEventListener("click",generateTodoForm);
-    const makeProject=document.createElement("p");
-    makeProject.textContent="Make New Project";
-    makeProject.addEventListener("click",loadProjectForm);
-    creationEl.append(makeTodo,makeProject);
+function generateOptions () {
+  clearElement(creationEl)
+  const makeTodo = document.createElement('p')
+  makeTodo.textContent = 'New Todo'
+  makeTodo.addEventListener('click', generateTodoForm)
+  const makeProject = document.createElement('p')
+  makeProject.textContent = 'Make New Project'
+  makeProject.addEventListener('click', loadProjectForm)
+  creationEl.append(makeTodo, makeProject)
 }
 
-generateOptions();
+generateOptions()
 
-function deleteThisProject(){
-    this.parentElement.remove();
-
+function deleteThisProject () {
+  this.parentElement.remove()
 }
 
+const getCircularReplacer = () => {
+  const seen = new WeakSet()
+  return (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return
+      }
+      seen.add(value)
+    }
+    return value
+  }
+}
 
+function loadSideBar () {
+  for (let i = 0; i < projects.length; i++) {
+    const projectDiv = document.createElement('div')
+    projectDiv.classList.add('project')
+    projectDiv.append(projects[i].domParagraph, projects[i].deleteButton)
+    projectsList.append(projectDiv)
+  }
+}
+
+loadSideBar()
